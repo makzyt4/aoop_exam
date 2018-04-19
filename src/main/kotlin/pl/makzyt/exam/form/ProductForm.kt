d72213d9bb55c1d8c020bbe93978afc86ec251b3
Maksymilian Zytkiewicz <maksymilian.zytkiewicz@gmail.com>
2018-04-19 12:23:29 +0200

    Added product form

---
+++ src/main/kotlin/pl/makzyt/exam/form/ProductForm.kt
@@ -0,0 +1,13 @@
+package pl.makzyt.exam.form
+
+import javax.validation.constraints.DecimalMin
+import javax.validation.constraints.Min
+
+class ProductForm {
+    @Min(value = 0, message = "{validation.invalidProductType}")
+    var typeId: Long = -1
+    @DecimalMin(value = "0.1", message = "{validation.minAmount}")
+    var amount: Float = 0F
+    @DecimalMin(value = "0.01", message = "{validation.minPrice}")
+    var price: Float = 0F
+}
